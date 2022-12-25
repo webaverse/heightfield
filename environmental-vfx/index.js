75ab4cb77ac7729c1773cd992790c50683c89ce6
tcm390 <tcm390@nyu.edu>
2022-12-25 15:53:30 -0500

    add grass

--- environmental-vfx/index.js
+++ environmental-vfx/index.js
@@ -3,6 +3,7 @@ import metaversefile from 'metaversefile';
 
 import { getFireflies } from './fireflies.js';
 import { getButterflies } from './butterflies.js';
+import { getLeaf } from './leaf.js';
 
 
 const {useLocalPlayer, useInternals} = metaversefile;
@@ -22,11 +23,13 @@ export class EnvironmentalVfx {
     this.fireflies = getFireflies(50, this.player,  this.camera);
     this.app.add(this.fireflies);
 
-
+    this.leaf = getLeaf(20, this.player);
+    this.app.add(this.leaf);
   }
   
   update(timestamp) {
     this.butterflies.update(timestamp);
     this.fireflies.update(timestamp);
+    this.leaf.update(timestamp);
   }
 }
